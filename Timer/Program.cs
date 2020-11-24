a3f875696ad80c2b1c0f3f44c43cf323ad6c1f45
Zbigniew Gawroński <zbigniew.gawronski@microsoft.wsei.edu.pl>
2020-11-24 23:45:20 +0100

    Add project files.

---
+++ Timer/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using TimerLib;
+
+namespace Timer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Time t = new Time(15, 20, 5);
+            Console.WriteLine(t.ToString());
+        }
+    }
+}
---
+++ TimerTest/TimerTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TimerTest
+{
+    [TestClass]
+    public class TimerTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
